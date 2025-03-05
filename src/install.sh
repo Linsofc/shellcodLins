edef8132c900c1239e99569869a90b4ddf75273f
Linsofc <137659454+Linsofc@users.noreply.github.com>
2025-03-05 16:33:24 +0700

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -6,6 +6,15 @@ YELLOW="\e[1;33m"
 GREEN="\e[1;32m"
 RED="\e[1;31m"
 RESET="\e[0m"
+# Update dan install paket yang dibutuhkan
+echo -e "\e[1;34m[INFO] Menginstall Dependensi...\e[0m"
+apt update && apt install -y curl neofetch
+
+# Bersihkan layar sebelum menampilkan banner
+clear
+# Menampilkan informasi sistem dengan neofetch
+
+neofetch --ascii_distro Debian --disable title uptime shell resolution de wm de_theme wm_theme icons terminal terminal_font gpu disk
 
 echo -e "${GREEN}[ NodeJs Berhasil Diinstall ]${RESET}"
 echo -e "${CYAN}Created By Lins Official${RESET}"
@@ -14,7 +23,7 @@ echo -e "${RED}YouTube:${RESET} https://youtube.com/@LinsOfficiall"
 echo -e "${GREEN}WhatsApp:${RESET} +6285190090045"
 echo -e "${BLUE}Saluran WhatsApp:${RESET} https://whatsapp.com/channel/0029VaeQHirJ93waiykxjF2L"
 echo -e "${YELLOW}Website Resmi:${RESET} https://linsofc.github.io"
-echo -e "============================================"
+echo -e "${RED}============================================${RESET}"
 echo -e "${RED}Harap berhati-hati terhadap akun palsu! Daftar di atas adalah akun resmi Lins Official.${RESET}"
 echo -e "${BLUE}© 2025${RESET}"
 
